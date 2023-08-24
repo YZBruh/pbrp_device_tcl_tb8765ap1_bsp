f012bb23e67811abc932013eef289ec6ced4f6a4
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-24 21:41:49 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,3 +7,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_tb8765ap1_bsp.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_tb8765ap1_bsp-user \
+    omni_tb8765ap1_bsp-userdebug \
+    omni_tb8765ap1_bsp-eng
