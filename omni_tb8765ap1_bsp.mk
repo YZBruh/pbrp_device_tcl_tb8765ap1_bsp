d47cac176046110a89367e16225bb5a55ffa7646
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-25 12:39:43 +0300

    Update omni_tb8765ap1_bsp.mk

--- omni_tb8765ap1_bsp.mk
+++ omni_tb8765ap1_bsp.mk
@@ -9,18 +9,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from tb8765ap1_bsp device
 $(call inherit-product, device/tcl/tb8765ap1_bsp/device.mk)
 
-BOARD_VENDOR := tcl
 PRODUCT_DEVICE := tb8765ap1_bsp
 PRODUCT_NAME := omni_tb8765ap1_bsp
 PRODUCT_BRAND := TCL
 PRODUCT_MODEL := 8088X
 PRODUCT_MANUFACTURER := tcl
-TARGET_VENDOR := tb8765ap1_bsp
 
 PRODUCT_GMS_CLIENTID_BASE := android-alcatel-rev1
 
