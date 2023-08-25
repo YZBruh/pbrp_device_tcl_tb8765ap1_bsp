6f8c37e9269ce3330c9787d4cfd7e3302fd168ab
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-25 18:25:59 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_DEVICE_VERSION := Build_by
 TW_DEFAULT_DEVICE_NAME := tb8765ap1_bsp
 TW_DEFAULT_LANGUAGE := en
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
